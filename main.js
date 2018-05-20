29494e10d6798cc4a586a4eff029676b65481f07
pavel-else <de.pavel.se@yandex.ru>
2018-05-21 01:53:28 +0300

    добавлен формат времени ms -> hh:mm:ss

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ new Vue({
 			let time = new Date();
 			this.order.time = time;
 			this.order.timeStart = time.toLocaleString();
-			this.order.timePlay = new Date() - time;
+			this.order.timePlay = 0;
 
 			this.orders.push(this.order);
 			this.clearOrder();
@@ -68,27 +68,43 @@ new Vue({
 		},
 		clearOrder() {
 			this.order = {
-			id: Number,
-			name: String,
-			customer: String,
-			time: Date,
-			timeStart: String,
-			timePlay: Date,
-			timeEnd: Number,
-			timeFull: Number,
-			bill: Boolean,
-			note: String,
-			saleId: Number,
-		}
-		}
+				id: Number,
+				name: String,
+				customer: String,
+				time: Date,
+				timeStart: String,
+				timePlay: Date,
+				timeEnd: Number,
+				timeFull: Number,
+				bill: Boolean,
+				note: String,
+				saleId: Number,
+			}
+		},
+		timeFormat (ms/**number*/){
+		    function num(val){
+		        val = Math.floor(val);
+		        return val < 10 ? '0' + val : val;
+		    }
+		    
+	        var sec = ms / 1000
+	          , hours = sec / 3600  % 24
+	          , minutes = sec / 60 % 60
+	          , seconds = sec % 60
+	        ;
+
+		    return num(hours) + ":" + num(minutes) + ":" + num(seconds);
+		},
 	},
 	created() {
 		this.products = this.getData();
 		//Нужно добавить обработку возможных ошибок
+		
 		setInterval(function(orders) {
 			for (let order = 0; order < orders.length; order++) {
 				orders[order].timePlay = new Date() - orders[order].time;
 			}
 		}, 1000, this.orders);
 	}
+
 })
