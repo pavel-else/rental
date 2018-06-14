b872ee861ce65188721fa3973c88b486c4e19c53
pavel-else <de.pavel.se@yandex.ru>
2018-06-14 13:51:26 +0300

    Рефакторинг кода

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template> 
     <div id="app"   class="tmp">
-        <product-list @edit="toEdit" class="tmp"></product-list>
+        <product-list class="tmp"></product-list>
 
         <new-order v-if="showNewOrder"></new-order>
 
@@ -31,101 +31,7 @@ export default {
 
     data () {
         return {
-            options: {
-                max_order_id: Number,
-            },
-
             now: Date,
-
-            orders: [],
-            products: [],
-            productsAll: [], 
-            customers: [],
-            product: {},
-            logs: '',
-        }
-    },
-
-    methods: {
-        closeModal() {
-            this.showOrderModal = false;
-        },
-
-        toEdit(item) {
-            this.product = item;
-            this.showOrderModal = true;
-        },
-
-        setOrder(order) {
-            this.setData('setOrder', order, response => {
-                console.log(response.data);
-                this.update();
-            });
- 
-            this.showOrderModal = false;
-        },
-
-        unset(item, index) {
-            this.products.push(item);
-            this.orders.splice(index, 1);
-        },
-        update() {
-            this.getData(['getProducts', 'getOrders', 'getMaxOrderID', 'getClients', 'getLogs'], response => {
-                this.options = response.data.options;           
-                this.productsAll = response.data.products;
-                this.products = this.filterProducts(this.productsAll);
-                this.orders = response.data.orders;
-                this.customers = response.data.clients;
-                this.logs = response.data.logs;
-                console.log(response.data.options.max_order_id);          
-            })
-        },
-        filterProducts(arr) {
-            let result = [];
-
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[i].active != 0) {
-                    result.push(arr[i]);
-                }
-            }
-
-            return result;
-        },
-        getData(cmds, callback) {
-            axios({
-                method: 'post',
-                url: 'http://overhost.net/rental2/api_v1/ajax/App/request.php',
-                data: {
-                    cmds: cmds,
-                }
-            })
-            .then(callback)
-        },
-        setData(cmd, value, callback) {
-            axios({
-                method: 'post',
-                url: 'http://overhost.net/rental2/api_v1/ajax/App/request.php',
-                data: {
-                    cmds: cmd,
-                    value: value,
-                }
-            })
-            .then(callback)
-        },
-
-        test() {
-            data = {
-                id: '155',
-                order_id: '777',
-                product_id: '16',
-                bill: 0,
-                bill_no_sale: 0,
-                end_time: 0,
-            }
-
-            this.setData('getOrderID', '678', response => {
-                console.log(response.data)
-            })
         }
     },
     computed: {
@@ -136,18 +42,11 @@ export default {
 
     created() {
         //Запрос данных для инициализации и обновления компонентов приложения
-        this.update();
         this.$store.dispatch('upd')
 
         // Обновление таймеров
         setInterval(() => {this.now = new Date()}, 1000)
     },
-
-    mounted() {
-        this.getData(['getClients'], response => {
-            this.customers = response.data.clients;
-        })
-    }
 }
 </script>
 
