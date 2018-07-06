97097ad0255902b7c80f4d6732bea832a5a9b904
pavel-else <de.pavel.se@yandex.ru>
2018-07-06 14:58:33 +0300

    Визуальное отображение тарифов

--- src/components/Options/Tariffs.vue
+++ src/components/Options/Tariffs.vue
@@ -1,19 +1,58 @@
 <template>
     <div class="option option-tariffs">
         <table>
+            <tr>
+                <th>id</th>
+                <th>Название</th>
+                <th>Тип</th>
+                <th>Расчасовка</th>
+                <th>Мин</th>
+                <th>Макс</th>
+                <th>Примечание</th>
+            </tr>
             <tr v-for="tariff in tariffs">
-                <td>{{ tariff }}</td>
+                <td>{{ tariff.id }}</td>
+                <td>{{ tariff.name }}</td>
+                <td>{{ tariff.type }}</td>
+                <td>{{ h(tariff) }}</td>
+                <td>{{ tariff.min }}</td>
+                <td>{{ tariff.max }}</td>
+                <td>{{ tariff.note }}</td>
             </tr>
         </table>
     </div>
 </template>
 <script>
     export default {
+        methods: {
+            h(tariff) {
+                const h = tariff.h
+
+                if (Object.keys(h).length === 0) return
+
+                let result = ''
+
+                for (let i in h) {
+                    result = result + `${h[i]} `
+                }
+
+                return result
+            }
+        },
         computed: {
             tariffs() {
                 return this.$store.getters.tariffsList
-            }
+            },
         }
         
     }
 </script>
+<style scoped>
+    td, th {
+        padding: 10px;
+    }
+    th {
+        text-align: left;
+    }
+    
+</style>
