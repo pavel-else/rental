2c301d952c6886f9dc050cffb160c1027545e458
pavel-else <de.pavel.se@yandex.ru>
2018-07-27 11:12:32 +0300

    fix roundBill

--- src/functions/roundBill.js
+++ src/functions/roundBill.js
@@ -3,7 +3,7 @@ import store from "../store"
 export default  function roundBill(bill) {
 	const round = store.getters.rent_round_bill
 
-    return Math.ceil(bill / round) * round
+    return round > 1 ? Math.ceil(bill / round) * round : Math.ceil(bill)
 }
 
 
