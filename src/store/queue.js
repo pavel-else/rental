7340ed18111f9eec630833e47404cf21e6677b95
pavel-else <de.pavel.se@yandex.ru>
2018-08-15 12:13:42 +0300

    init addToQueue

--- src/store/queue.js
+++ src/store/queue.js
@@ -3,13 +3,21 @@ export default {
 		queue: []
 	},
 	mutations: {
-		addToQueue(state) {
-
+		addToQueue(state, cell) {
+			state.queue.push(cell)
 		},
 		sendQueue(state) {
-
+			//send queue
+			console.log(state.queue)
 		}
 	},
-	actions: {},
+	actions: {
+		addToQueue({commit}, {cmd, value}) {
+			if (!cmd) {
+				console.log('empty cmd')
+				return
+			}
+		}
+	},
 	getters: {}
 }
