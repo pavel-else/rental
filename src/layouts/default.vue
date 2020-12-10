0edd0cea2290158ea440a037c7c0bee64c3b6a84
pavel-else <de.pavel.se@yandex.ru>
2020-12-10 17:12:15 +0300

    tmp

---
+++ src/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page page--default">
+    <slot />
+  </div>
+</template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="main">main</div>
+  <default-layout>
+    <v-container fluid>
+      <v-row>
+        <v-col>asdasdf</v-col>
+        <v-col>412341234</v-col>
+      </v-row>
+    </v-container>
+  </default-layout>
 </template>
 
 <script>
+import DefaultLayout from '@/layouts/default';
+
 export default {
   name: 'Main',
+  components: {
+    DefaultLayout,
+  }
 }
 </script>
